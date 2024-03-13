6f72c2d3bba3d9738c929c41e1ec53efc79eec84
121140180_Daniel <118008459+NielDiaRus@users.noreply.github.com>
2024-03-13 22:00:35 +0700

    Create  MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,17 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        btnSubmit.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
